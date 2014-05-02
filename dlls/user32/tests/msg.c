2029d5ad2ed3d65839819852009c29f576e7e4f0
Michael Müller <michael@fds-team.de>
2014-05-02 20:46:19 +0200

    user32: Decrease minimum SetTimer interval to 5 ms. (try 2)

--- dlls/user32/tests/msg.c
+++ dlls/user32/tests/msg.c
@@ -10309,6 +10309,7 @@ static void test_timers(void)
     start = GetTickCount();
     while (GetTickCount()-start < 1001 && GetMessageA(&msg, info.hWnd, 0, 0))
         DispatchMessageA(&msg);
+todo_wine
     ok(abs(count-TIMER_COUNT_EXPECTED) < TIMER_COUNT_TOLERANCE /* xp */
        || broken(abs(count-64) <= TIMER_COUNT_TOLERANCE) /* most common */
        || broken(abs(count-43) <= TIMER_COUNT_TOLERANCE) /* w2k3, win8 */,
@@ -10379,6 +10380,7 @@ static void test_timers_no_wnd(void)
     start = GetTickCount();
     while (GetTickCount()-start < 1001 && GetMessageA(&msg, NULL, 0, 0))
         DispatchMessageA(&msg);
+todo_wine
     ok(abs(count-TIMER_COUNT_EXPECTED) < TIMER_COUNT_TOLERANCE /* xp */
        || broken(abs(count-64) <= TIMER_COUNT_TOLERANCE) /* most common */
        || broken(abs(count-43) <= TIMER_COUNT_TOLERANCE) /* w1064v1809 */,
