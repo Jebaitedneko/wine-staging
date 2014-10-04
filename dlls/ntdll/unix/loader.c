c9ca96573a7012e2a636e9fa3e6d9ae757b1e4c5
Michael Müller <michael@fds-team.de>
2014-10-04 02:53:22 +0200

    ntdll: Setup a temporary signal handler during process startup to handle page faults. (v2)

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -1905,6 +1905,8 @@ void __wine_main( int argc, char *argv[], char *envp[] )
 #endif
 
     virtual_init();
+    signal_init_early();
+
     init_environment( argc, argv, envp );
 
 #ifdef __APPLE__
--- dlls/ntdll/unix/signal_x86_64.c
+++ dlls/ntdll/unix/signal_x86_64.c
@@ -2767,6 +2767,12 @@ void signal_init_process(void)
     exit(1);
 }
 
+/**********************************************************************
+ *    signal_init_early
+ */
+void signal_init_early(void)
+{
+}
 
 /***********************************************************************
  *           init_thread_context
