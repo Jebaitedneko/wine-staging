bef61205dbcffc866515cc7b9dd29fd938d14d5d
Mark Harmstone <mark@harmstone.com>
2015-03-27 20:48:19 +0000

    dsound: Support getting and setting EAX buffer properties.

--- dlls/dsound/dsound_private.h
+++ dlls/dsound/dsound_private.h
@@ -172,6 +172,8 @@ struct IDirectSoundBufferImpl
     int                         num_filters;
     DSFilter*                   filters;
 
+    eax_buffer_info             eax;
+
     struct list entry;
 };
 
