d1138bbbd1bf91a634ac0208fa619f044d5a97f1
Mark Harmstone <mark@harmstone.com>
2015-04-05 14:16:11 +0100

    winecfg: Add checkbox to enable/disable EAX support.

--- programs/winecfg/staging.c
+++ programs/winecfg/staging.c
@@ -58,10 +58,27 @@ static void vaapi_set(BOOL status)
     set_reg_key(config_key, keypath(L"DXVA2"), L"backend", status ? L"va" : NULL);
 }
 
+/*
+ * EAX
+ */
+static BOOL eax_get(void)
+{
+    BOOL ret;
+    WCHAR *value = get_reg_key(config_key, keypath(L"DirectSound"), L"EAXEnabled", L"N");
+    ret = IS_OPTION_TRUE(*value);
+    HeapFree(GetProcessHeap(), 0, value);
+    return ret;
+}
+static void eax_set(BOOL status)
+{
+    set_reg_key(config_key, keypath(L"DirectSound"), L"EAXEnabled", status ? L"Y" : L"N");
+}
+
 static void load_staging_settings(HWND dialog)
 {
     CheckDlgButton(dialog, IDC_ENABLE_CSMT, csmt_get() ? BST_CHECKED : BST_UNCHECKED);
     CheckDlgButton(dialog, IDC_ENABLE_VAAPI, vaapi_get() ? BST_CHECKED : BST_UNCHECKED);
+    CheckDlgButton(dialog, IDC_ENABLE_EAX, eax_get() ? BST_CHECKED : BST_UNCHECKED);
 }
 
 INT_PTR CALLBACK StagingDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -93,6 +110,10 @@ INT_PTR CALLBACK StagingDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lPar
             return TRUE;
         case IDC_ENABLE_VAAPI:
             vaapi_set(IsDlgButtonChecked(hDlg, IDC_ENABLE_VAAPI) == BST_CHECKED);
+            SendMessageW(GetParent(hDlg), PSM_CHANGED, 0, 0);
+            return TRUE;
+        case IDC_ENABLE_EAX:
+            eax_set(IsDlgButtonChecked(hDlg, IDC_ENABLE_EAX) == BST_CHECKED);
             SendMessageW(GetParent(hDlg), PSM_CHANGED, 0, 0);
             return TRUE;
         }
