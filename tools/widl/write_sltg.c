c0807a9922bccc38203c5f9d6ea5c7d4e8d22abc
Dmitry Timoshkov <dmitry@baikal.ru>
2016-01-10 16:01:55 +0800

    widl: Write correct syskind by SLTG typelib generator.

--- tools/widl/write_sltg.c
+++ tools/widl/write_sltg.c
@@ -232,7 +232,7 @@ static void init_library(struct sltg_typelib *sltg)
     sltg->library.name = add_name(sltg, sltg->typelib->name);
     sltg->library.helpstring = NULL;
     sltg->library.helpcontext = 0;
-    sltg->library.syskind = SYS_WIN32;
+    sltg->library.syskind = (pointer_size == 8) ? SYS_WIN64 : SYS_WIN32;
     sltg->library.lcid = 0x0409;
     sltg->library.libflags = 0;
     sltg->library.version = 0;
