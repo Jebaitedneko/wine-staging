fba72de5a3dd905125f80ba910334adad02fe050
Sebastian Lackner <sebastian@fds-team.de>
2017-09-28 07:02:55 +0200

    include: Always define hton/ntoh macros.

--- include/winsock.h
+++ include/winsock.h
@@ -473,8 +473,6 @@ int WINAPI __WSAFDIsSet(SOCKET,WS(fd_set)*);
 #endif /* WS_DEFINE_SELECT */
 
 /* we have to define hton/ntoh as macros to avoid conflicts with Unix headers */
-#ifndef USE_WS_PREFIX
-
 #undef htonl
 #undef htons
 #undef ntohl
@@ -511,8 +509,6 @@ static inline ULONG __wine_ulong_swap(ULONG l)
 #define ntohs __wine_ushort_swap
 
 #endif  /* WORDS_BIGENDIAN */
-
-#endif  /* USE_WS_PREFIX */
 
 /*
  * Internet address (old style... should be updated)
