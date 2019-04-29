c69cb58d740246b0761a17edf28013b3c87bdd68
Jetro Jormalainen <jje-wine@jv.jetro.fi>
2019-04-30 09:21:24 +1000

    dinput: Support username in Config dialog.

--- dlls/dinput/dinput_private.h
+++ dlls/dinput/dinput_private.h
@@ -72,6 +72,7 @@ extern void check_dinput_hooks( IDirectInputDevice8W *iface, BOOL acquired ) DEC
 extern void check_dinput_events(void) DECLSPEC_HIDDEN;
 
 extern HRESULT _configure_devices(IDirectInput8W *iface, LPDICONFIGUREDEVICESCALLBACK lpdiCallback, LPDICONFIGUREDEVICESPARAMSW lpdiCDParams, DWORD dwFlags, LPVOID pvRefData) DECLSPEC_HIDDEN;
+extern HRESULT save_mapping_settings(IDirectInputDevice8W *iface, LPDIACTIONFORMATW lpdiaf, LPCWSTR lpszUsername) DECLSPEC_HIDDEN;
 
 extern WCHAR* get_mapping_path(const WCHAR *device, const WCHAR *username) DECLSPEC_HIDDEN;
 extern DWORD get_device_type(DWORD version, BOOL is_joystick) DECLSPEC_HIDDEN;
