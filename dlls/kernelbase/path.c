e9fd25ae37e9ee51ab72768a5f82e126071fda39
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2019-06-28 09:57:31 +1000

    shlwapi: Support ./ in UrlCanonicalize.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=23166
    Signed-off-by: Alistair Leslie-Hughes <leslie_alistair@hotmail.com>

--- dlls/kernelbase/path.c
+++ dlls/kernelbase/path.c
@@ -3577,6 +3577,7 @@ HRESULT WINAPI UrlCanonicalizeW(const WCHAR *src_url, WCHAR *canonicalized, DWOR
      *         4   have //  5,3
      *         5   have 1[+] alnum  6,3
      *         6   have location (found /) save root location
+     *         7   have ./
      */
 
     wk1 = url;
@@ -3601,6 +3602,11 @@ HRESULT WINAPI UrlCanonicalizeW(const WCHAR *src_url, WCHAR *canonicalized, DWOR
     else if (url[0] == '/')
     {
         state = 5;
+        is_file_url = TRUE;
+    }
+    else if (url[0] == '.' && url[1] == '/')
+    {
+        state = 7;
         is_file_url = TRUE;
     }
 
@@ -3806,6 +3812,15 @@ HRESULT WINAPI UrlCanonicalizeW(const WCHAR *src_url, WCHAR *canonicalized, DWOR
                 }
             }
             *wk2 = '\0';
+            break;
+        case 7:
+            if (flags & URL_DONT_SIMPLIFY)
+            {
+                state = 3;
+                break;
+            }
+            wk1 += 2;
+            state = 6;
             break;
         default:
             FIXME("how did we get here - state=%d\n", state);
