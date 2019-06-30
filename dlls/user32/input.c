910a9012fce2509e70fae0affc366d0a97402544
Austin English <austinenglish@gmail.com>
2019-07-01 09:17:31 +1000

    user32: Added LoadKeyboardLayoutEx stub.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=28170

--- dlls/user32/input.c
+++ dlls/user32/input.c
@@ -1012,6 +1012,15 @@ HKL WINAPI LoadKeyboardLayoutA(LPCSTR pwszKLID, UINT Flags)
     return ret;
 }
 
+/***********************************************************************
+ *              LoadKeyboardLayoutEx (USER32.@)
+ */
+HKL WINAPI LoadKeyboardLayoutEx(DWORD unknown, const WCHAR *locale, UINT flags)
+{
+    FIXME("(%d, %s, %x) semi-stub!\n", unknown, debugstr_w(locale), flags);
+    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+    return LoadKeyboardLayoutW(locale, flags);
+}
 
 /***********************************************************************
  *		UnloadKeyboardLayout (USER32.@)
