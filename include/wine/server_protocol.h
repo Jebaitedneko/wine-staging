dd0313a82244c8b05bbe216a4cc05b854cd5c001
Rémi Bernon <rbernon@codeweavers.com>
2019-08-26 14:37:20 +0200

    server: Add send_hardware_message flags for rawinput translation.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -2715,6 +2715,8 @@ struct send_hardware_message_reply
     char __pad_28[4];
 };
 #define SEND_HWMSG_INJECTED    0x01
+#define SEND_HWMSG_RAWINPUT    0x02
+#define SEND_HWMSG_WINDOW      0x04
 
 
 
@@ -6360,7 +6362,7 @@ union generic_reply
 
 /* ### protocol_version begin ### */
 
-#define SERVER_PROTOCOL_VERSION 665
+#define SERVER_PROTOCOL_VERSION 666
 
 /* ### protocol_version end ### */
 
