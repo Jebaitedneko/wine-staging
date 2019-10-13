5abc39128d4d1b0e137a3f04ba281e97a2417210
David Torok <dt@zeroitlab.com>
2019-10-14 10:37:49 +1100

    user32: AddInternalGetWindowIcon stub.

--- dlls/user32/win.c
+++ dlls/user32/win.c
@@ -2886,6 +2886,13 @@ INT WINAPI GetWindowTextA( HWND hwnd, LPSTR lpString, INT nMaxCount )
     return strlen(lpString);
 }
 
+/*******************************************************************
+ *		InternalGetWindowIcon (USER32.@)
+ */
+INT WINAPI InternalGetWindowIcon(HWND hwnd, UINT iconType )
+{
+    return NULL;
+}
 
 /*******************************************************************
  *		InternalGetWindowText (USER32.@)
