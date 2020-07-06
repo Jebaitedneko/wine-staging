80bd65c935540605ee6938a202e194f65320d20f
Zebediah Figura <zfigura@codeweavers.com>
2020-07-06 15:19:37 -0500

    ntdll, server: Implement waiting on server-bound objects.
    The code here is sort of self-explanatory, but since I split it up over
    several patches I'll provide a quick explanation. The basic principle is
    that we can create an eventfd descriptor for any synchronizable handle, and
    signal it on the server side whenever a wakeup would be triggered. This means
    not only that we can wait simultaneously on esync primitives and on other
    primitives, but that we can do it all in "user-mode", i.e. without having to
    make a server call.
    
    With this patch we break waiting on svcctl.exe.

--- server/thread.c
+++ server/thread.c
@@ -51,6 +51,7 @@
 #include "request.h"
 #include "user.h"
 #include "security.h"
+#include "esync.h"
 
 
 #ifdef __i386__
@@ -1048,6 +1049,9 @@ void wake_up( struct object *obj, int max )
 {
     struct list *ptr;
     int ret;
+
+    if (do_esync())
+        esync_wake_up( obj );
 
     LIST_FOR_EACH( ptr, &obj->wait_queue )
     {
