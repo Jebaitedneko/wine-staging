gena-moscow/wine-patched

winecfg: Add checkbox to enable/disable EAX support.

programs/winecfg/staging.c
@@ -71,10 +71,27 @@ static void vaapi_set(BOOL status)
 #endif
 }
 
+/*
+ * EAX
+ */
+static BOOL eax_get(void)
+{
+    BOOL ret;
+    char *value = get_reg_key(config_key, keypath("DirectSound"), "EAXEnabled", "Y");
+    ret = IS_OPTION_TRUE(*value);
+    HeapFree(GetProcessHeap(), 0, value);
+    return ret;
+}
+static void eax_set(BOOL status)
+{
+    set_reg_key(config_key, keypath("DirectSound"), "EAXEnabled", status ? "Y" : "N");
+}
+
 static void load_staging_settings(HWND dialog)
 {
     CheckDlgButton(dialog, IDC_ENABLE_CSMT, csmt_get() ? BST_CHECKED : BST_UNCHECKED);
     CheckDlgButton(dialog, IDC_ENABLE_VAAPI, vaapi_get() ? BST_CHECKED : BST_UNCHECKED);
+    CheckDlgButton(dialog, IDC_ENABLE_EAX, eax_get() ? BST_CHECKED : BST_UNCHECKED);
 
 #ifndef HAVE_VAAPI
     disable(IDC_ENABLE_VAAPI);
@@ -110,6 +127,10 @@ INT_PTR CALLBACK StagingDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lPar
             return TRUE;
         case IDC_ENABLE_VAAPI:
             vaapi_set(IsDlgButtonChecked(hDlg, IDC_ENABLE_VAAPI) == BST_CHECKED);
+            SendMessageW(GetParent(hDlg), PSM_CHANGED, 0, 0);
+            return TRUE;
+        case IDC_ENABLE_EAX:
+            eax_set(IsDlgButtonChecked(hDlg, IDC_ENABLE_EAX) == BST_CHECKED);
             SendMessageW(GetParent(hDlg), PSM_CHANGED, 0, 0);
             return TRUE;
         }

Jebaitedneko/wine-staging

programs/winecfg/staging.c
@@ -67,10 +67,27 @@ static void vaapi_set(BOOL status)
 #endif
 }
 
+/*
+ * EAX
+ */
+static BOOL eax_get(void)
+{
+    BOOL ret;
+    char *value = get_reg_key(config_key, keypath("DirectSound"), "EAXEnabled", "N");
+    ret = IS_OPTION_TRUE(*value);
+    HeapFree(GetProcessHeap(), 0, value);
+    return ret;
+}
+static void eax_set(BOOL status)
+{
+    set_reg_key(config_key, keypath("DirectSound"), "EAXEnabled", status ? "Y" : "N");
+}
+
 static void load_staging_settings(HWND dialog)
 {
     CheckDlgButton(dialog, IDC_ENABLE_CSMT, csmt_get() ? BST_CHECKED : BST_UNCHECKED);
     CheckDlgButton(dialog, IDC_ENABLE_VAAPI, vaapi_get() ? BST_CHECKED : BST_UNCHECKED);
+    CheckDlgButton(dialog, IDC_ENABLE_EAX, eax_get() ? BST_CHECKED : BST_UNCHECKED);
 
 #ifndef HAVE_VAAPI
     disable(IDC_ENABLE_VAAPI);
@@ -106,6 +123,10 @@ INT_PTR CALLBACK StagingDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lPar
             return TRUE;
         case IDC_ENABLE_VAAPI:
             vaapi_set(IsDlgButtonChecked(hDlg, IDC_ENABLE_VAAPI) == BST_CHECKED);
+            SendMessageW(GetParent(hDlg), PSM_CHANGED, 0, 0);
+            return TRUE;
+        case IDC_ENABLE_EAX:
+            eax_set(IsDlgButtonChecked(hDlg, IDC_ENABLE_EAX) == BST_CHECKED);
             SendMessageW(GetParent(hDlg), PSM_CHANGED, 0, 0);
             return TRUE;
         }