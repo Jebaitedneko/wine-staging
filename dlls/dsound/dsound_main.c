gena-moscow/wine-patched

dsound: Allow disabling of EAX support in the registry.
Based on a patch by Mark Harmstone.

dlls/dsound/dsound_main.c
@@ -95,7 +95,11 @@ WCHAR wine_vxd_drv[] = { 'w','i','n','e','m','m','.','v','x','d', 0 };
 int ds_hel_buflen = 32768 * 2;
 int ds_snd_queue_max = 10;
 int ds_hq_buffers_max = 4;
+BOOL ds_eax_enabled = TRUE;
 static HINSTANCE instance;
+
+#define IS_OPTION_TRUE(ch) \
+    ((ch) == 'y' || (ch) == 'Y' || (ch) == 't' || (ch) == 'T' || (ch) == '1')
 
 /*
  * Get a config key from either the app-specific or the default config
@@ -108,7 +112,6 @@ static inline DWORD get_config_key( HKEY defkey, HKEY appkey, const char *name,
     if (defkey && !RegQueryValueExA( defkey, name, 0, NULL, (LPBYTE)buffer, &size )) return 0;
     return ERROR_FILE_NOT_FOUND;
 }
-
 
 /*
  * Setup the dsound options.
@@ -150,9 +153,11 @@ void setup_dsound_options(void)
     if (!get_config_key( hkey, appkey, "SndQueueMax", buffer, MAX_PATH ))
         ds_snd_queue_max = atoi(buffer);
 
-
     if (!get_config_key( hkey, appkey, "HQBuffersMax", buffer, MAX_PATH ))
         ds_hq_buffers_max = atoi(buffer);
+
+    if (!get_config_key( hkey, appkey, "EAXEnabled", buffer, MAX_PATH ))
+        ds_eax_enabled = IS_OPTION_TRUE( buffer[0] );
 
     if (appkey) RegCloseKey( appkey );
     if (hkey) RegCloseKey( hkey );
@@ -160,6 +165,7 @@ void setup_dsound_options(void)
     TRACE("ds_hel_buflen = %d\n", ds_hel_buflen);
     TRACE("ds_snd_queue_max = %d\n", ds_snd_queue_max);
     TRACE("ds_hq_buffers_max = %d\n", ds_hq_buffers_max);
+    TRACE("ds_eax_enabled = %u\n", ds_eax_enabled);
 }
 
 static const char * get_device_id(LPCGUID pGuid)

Jebaitedneko/wine-staging

dlls/dsound/dsound_main.c
@@ -93,6 +93,10 @@ const WCHAR wine_vxd_drv[] = L"winemm.vxd";
 /* All default settings, you most likely don't want to touch these, see wiki on UsefulRegistryKeys */
 int ds_hel_buflen = 32768 * 2;
 int ds_hq_buffers_max = 4;
+BOOL ds_eax_enabled = FALSE;
+
+#define IS_OPTION_TRUE(ch) \
+    ((ch) == 'y' || (ch) == 'Y' || (ch) == 't' || (ch) == 'T' || (ch) == '1')
 
 /*
  * Get a config key from either the app-specific or the default config
@@ -147,11 +151,15 @@ void setup_dsound_options(void)
     if (!get_config_key( hkey, appkey, "HQBuffersMax", buffer, MAX_PATH ))
         ds_hq_buffers_max = atoi(buffer);
 
+    if (!get_config_key( hkey, appkey, "EAXEnabled", buffer, MAX_PATH ))
+        ds_eax_enabled = IS_OPTION_TRUE( buffer[0] );
+
     if (appkey) RegCloseKey( appkey );
     if (hkey) RegCloseKey( hkey );
 
     TRACE("ds_hel_buflen = %d\n", ds_hel_buflen);
     TRACE("ds_hq_buffers_max = %d\n", ds_hq_buffers_max);
+    TRACE("ds_eax_enabled = %u\n", ds_eax_enabled);
 }
 
 static const char * get_device_id(LPCGUID pGuid)