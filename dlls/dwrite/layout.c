lucianposton/wine

dwrite: Use MapCharacters for dummy line metrics
Fixes: https://bugs.winehq.org/show_bug.cgi?id=44052

Signed-off-by: Lucian Poston <lucianposton@pm.me>

dlls/dwrite/layout.c
@@ -1808,8 +1808,11 @@ static HRESULT layout_set_dummy_line_metrics(struct dwrite_textlayout *layout, U
     DWRITE_FONT_METRICS fontmetrics;
     struct layout_range *range;
     IDWriteFontFace *fontface;
+    IDWriteFontFallback *fallback;
     IDWriteFont *font;
     HRESULT hr;
+    UINT32 mapped_length;
+    FLOAT scale;
 
     range = get_layout_range_by_pos(layout, pos);
     hr = create_matching_font(range->collection,
@@ -1818,8 +1821,34 @@ static HRESULT layout_set_dummy_line_metrics(struct dwrite_textlayout *layout, U
         range->style,
         range->stretch,
         &font);
+
+    if (FAILED(hr)) {
+        if (layout->format.fallback) {
+            fallback = layout->format.fallback;
+            IDWriteFontFallback_AddRef(fallback);
+        } else if (FAILED(hr = IDWriteFactory5_GetSystemFontFallback(layout->factory, &fallback))) {
+            WARN("Failed to get system fallback, hr %#x.\n", hr);
+            return hr;
+        }
+
+        hr = IDWriteFontFallback_MapCharacters(fallback,
+                (IDWriteTextAnalysisSource *)&layout->IDWriteTextAnalysisSource1_iface,
+                pos,
+                layout->len,
+                range->collection,
+                range->fontfamily,
+                range->weight,
+                range->style,
+                range->stretch,
+                &mapped_length,
+                &font,
+                &scale);
+        IDWriteFontFallback_Release(fallback);
+    }
     if (FAILED(hr))
         return hr;
+    if (font == NULL)
+        return S_OK;
     hr = IDWriteFont_CreateFontFace(font, &fontface);
     IDWriteFont_Release(font);
     if (FAILED(hr))

Jebaitedneko/wine-staging

dlls/dwrite/layout.c
@@ -1838,8 +1838,11 @@ static HRESULT layout_set_dummy_line_metrics(struct dwrite_textlayout *layout, U
     DWRITE_FONT_METRICS fontmetrics;
     struct layout_range *range;
     IDWriteFontFace *fontface;
+    IDWriteFontFallback *fallback;
     IDWriteFont *font;
     HRESULT hr;
+    UINT32 mapped_length;
+    FLOAT scale;
 
     range = get_layout_range_by_pos(layout, pos);
     hr = create_matching_font(range->collection,
@@ -1848,8 +1851,34 @@ static HRESULT layout_set_dummy_line_metrics(struct dwrite_textlayout *layout, U
         range->style,
         range->stretch,
         &font);
+
+    if (FAILED(hr)) {
+        if (layout->format.fallback) {
+            fallback = layout->format.fallback;
+            IDWriteFontFallback_AddRef(fallback);
+        } else if (FAILED(hr = IDWriteFactory7_GetSystemFontFallback(layout->factory, &fallback))) {
+            WARN("Failed to get system fallback, hr %#x.\n", hr);
+            return hr;
+        }
+
+        hr = IDWriteFontFallback_MapCharacters(fallback,
+                (IDWriteTextAnalysisSource *)&layout->IDWriteTextAnalysisSource1_iface,
+                pos,
+                layout->len,
+                range->collection,
+                range->fontfamily,
+                range->weight,
+                range->style,
+                range->stretch,
+                &mapped_length,
+                &font,
+                &scale);
+        IDWriteFontFallback_Release(fallback);
+    }
     if (FAILED(hr))
         return hr;
+    if (font == NULL)
+        return S_OK;
     hr = IDWriteFont_CreateFontFace(font, &fontface);
     IDWriteFont_Release(font);
     if (FAILED(hr))